d761f610ec43beec816412baa70637d7bb4188ef
Asus <joshuapaquino27@gmail.com>
2023-03-07 19:56:00 +0800

    MainActivity.java

--- app/src/main/java/com/example/hyst/SignUp.java
+++ app/src/main/java/com/example/hyst/SignUp.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignUp extends AppCompatActivity {
 
+    DatabaseReference myDB = FirebaseDatabase.getInstance().getReferenceFromUrl("https://hyst-f90db-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
