63dd078e7b23ebaa5016745ff8f1dcc0e8bfb992
Asus <joshuapaquino27@gmail.com>
2023-03-07 17:32:09 +0800

    MainActivity.java

--- app/src/main/java/com/example/hyst/MainActivity.java
+++ app/src/main/java/com/example/hyst/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.hyst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
     }
 }
