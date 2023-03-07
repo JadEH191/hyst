c71b54870b5b26bc34b5d61d73753ae45787eba5
Asus <joshuapaquino27@gmail.com>
2023-03-07 19:40:55 +0800

    MainActivity.java

--- app/src/main/java/com/example/hyst/MainActivity.java
+++ app/src/main/java/com/example/hyst/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     public void Login() {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
