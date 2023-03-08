7ff02aa94c3e29923ee48333fab8058813b36061
JadEH191 <95281160+JadEH191@users.noreply.github.com>
2023-03-08 13:31:08 +0800

    calcu

--- app/src/main/java/com/example/hyst/Login.java
+++ app/src/main/java/com/example/hyst/Login.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,6 +33,7 @@ TextView aSignup;
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
         aSignup = findViewById(R.id.aSignUp);
+        aSignup.setPaintFlags(aSignup.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
         aSignup.setOnClickListener(new View.OnClickListener() {
             @Override
